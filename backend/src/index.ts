704376545e2a97225e70a5aae157f5d61edd3ddb
Lazar Kojic <lazar.kojic@frontmendev.com>
2024-10-20 01:23:11 +0200

    Socket.io configurated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,34 @@
-// src/index.ts
-import express, { Request, Response, NextFunction } from "express";
-import asyncHandler from "./utils/catchAsync";
-import { createServer } from "node:http";
+import express from "express";
+import { createServer } from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-// Middleware
-app.use(express.json());
-
-// Example route
-app.get(
-  "/",
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("Before");
-    next(); // Ensure to call next() to proceed to the next handler
+const io = new Server(server, {
+  cors: {
+    origin: process.env.NEXT_SERVER_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
   },
-  asyncHandler(async (req: Request, res: Response) => {
-    // Async handler, safely wrapped with asyncHandler to catch errors
-    res.send("Hello, TypeScript with Express!");
-  })
-);
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("message", (msg) => {
+    console.log("Message received: " + msg);
+    io.emit("message", "Whats up frontend?");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
